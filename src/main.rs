bebb930e09d145fdaf990f39434a833e0712082a
Aran Varun <vir@tue.su>
2025-04-04 11:25:42 +0300

    params

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,18 @@ use std::fs::File;
 use std::io::Write;
 
 fn main() {
+    // Get stdin parameters
+    let mut args = std::env::args();
+    println!("{:?}", args);
+    // Get first argument (program name)
+    args.next().expect("No arguments");
+    let output_fname = args.next().expect("No arguments");
+    let input_string = args.next().expect("No arguments");
+
     // Open file for writing
-    let mut file = File::create("sess.b64url_decoded.bin").expect("Unable to create file");
+    let mut file = File::create(&output_fname).expect("Unable to create file");
     // println!("{:?}", URL_SAFE_NO_PAD.decode(b"_4TUgAAz5jqmpfS0a8to2g"));
-    file.write(&URL_SAFE_NO_PAD.decode(b"_4TUgAAz5jqmpfS0a8to2g").unwrap())
+    // file.write(&URL_SAFE_NO_PAD.decode(&input_string).unwrap())
+    file.write(&URL_SAFE_NO_PAD.decode(&input_string.as_bytes()).unwrap())
         .expect("Unable to write");
 }
