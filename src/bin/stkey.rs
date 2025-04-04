f22c9d1e5fd6344be9146aa58c0f744cb375b688
Aran Varun <vir@tue.su>
2025-04-04 16:12:01 +0300

    writes random 16 bytes into file named as base64uri of these bytes

--- src/bin/stkey.rs
+++ src/bin/stkey.rs
@@ -1,8 +1,16 @@
 use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine as _};
 use rand::{rng, RngCore};
+use std::fs::File;
+use std::io::Write;
 
 fn main() {
     let mut data = [0u8; 16];
     rng().fill_bytes(&mut data);
-    println!("{:?}", URL_SAFE_NO_PAD.encode(data));
+    // println!("{:?}", URL_SAFE_NO_PAD.encode(data));
+    let fname = URL_SAFE_NO_PAD.encode(data);
+    // Create file for writing
+    let mut file = File::create(format!("./{}.bin", &fname)).expect("Unable to create file");
+
+    // Write the data to the file
+    file.write(&data).expect("Unable to write to file");
 }
